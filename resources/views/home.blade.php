693bbb27f9a78130f77e0650c5d595073b74a410
yyussrii <yusriappolo@gmail.con>
2022-06-23 08:58:58 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@800&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;800&display=swap" rel="stylesheet">
+    <style>
+        body{
+            background-color: #1f1f1f;
+        }
+        .hero{
+            font-family: 'Poppins', sans-serif;;
+            color: #1f1f1f;
+            margin-top: 5%;
+            background-color: #ffffff;
+            width: 500px;
+            border-radius: 20px;
+        }
+        .content{
+            width: 1100px;
+            margin: auto;
+            margin-top: 30px;
+            box-shadow: 0 10px 10px black;
+            background-color: #ffffff;    
+            border-radius: 10px;
+            font-family: 'Poppins';
+        }
+
+        h4 {
+            font-family: "Poppins";
+        }
+        h5 {
+            font-family: "Poppins";
+        }
+    </style>
+</head>
+<body>
+    <center>
+        <h1 class="hero p-3">Biodata Yusri Rizqi</h1>
+        <h4 class="text-light">Contoh website ini dibuat untuk memenuhi tugas Pak Andri</h4>
+        <h5 class="text-light">Mohon maaf atas Keterlambatannya</h5>
+    </center>
+    <div class="content p-3">
+        <h3>Nama    :Yusri Rizqi</h3>
+        <h3>Jenis kelamin   : Laki-laki</h3>
+        <h3>TTL     : Semarang, 17 Agustus 2004</h3>
+        <h3>Hobi    : Main Game, Tidur</h3>
+        <h3>Jurusan : Sistem Informasi Jaringan dan Aplikasi</h3>
+    </div>
+
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+</html>
